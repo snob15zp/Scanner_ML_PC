2b6b63ca6d1282178834bddf57754fc64a7c11a6
Alexander Baltser <aubalcer@gmail.com>
2023-05-08 14:59:58 +0300

    Add test application

---
+++ ConsoleApplication1/generation_test_signal.h
@@ -0,0 +1,32 @@
+/*
+ * File: generation_test_signal.h
+ *
+ * MATLAB Coder version            : 3.3
+ * C/C++ source code generated on  : 07-May-2023 21:19:13
+ */
+
+#ifndef GENERATION_TEST_SIGNAL_H
+#define GENERATION_TEST_SIGNAL_H
+
+/* Include Files */
+#include <float.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rt_nonfinite.h"
+#include "rtwtypes.h"
+#include "generation_test_signal_types.h"
+
+/* Function Declarations */
+extern void generation_test_signal(double Tm, double Fd, double mz, double Ak,
+  double A1, double A2, double F1, double F2, double P1, double P2, double An,
+  emxArray_real_T *Signal, emxArray_real_T *T, double *FftL);
+
+#endif
+
+/*
+ * File trailer for generation_test_signal.h
+ *
+ * [EOF]
+ */
---
+++ ConsoleApplication1/generation_test_signal_terminate.h
@@ -0,0 +1,30 @@
+/*
+ * File: generation_test_signal_terminate.h
+ *
+ * MATLAB Coder version            : 3.3
+ * C/C++ source code generated on  : 07-May-2023 21:19:13
+ */
+
+#ifndef GENERATION_TEST_SIGNAL_TERMINATE_H
+#define GENERATION_TEST_SIGNAL_TERMINATE_H
+
+/* Include Files */
+#include <float.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rt_nonfinite.h"
+#include "rtwtypes.h"
+#include "generation_test_signal_types.h"
+
+/* Function Declarations */
+extern void generation_test_signal_terminate(void);
+
+#endif
+
+/*
+ * File trailer for generation_test_signal_terminate.h
+ *
+ * [EOF]
+ */
